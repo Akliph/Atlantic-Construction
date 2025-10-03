ad4f4d9f5d6123cebd6e2350bf2caf351b75f4ca
Alex G <alexgiberson7@gmail.com>
2025-10-02 20:15:08 -0400

    initial commit

---
+++ public/main.js
@@ -0,0 +1,8 @@
+console.log("JS loaded");
+
+const header = document.getElementById("site-header");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 0) {header.classList.add("scrolled")}
+    else {header.classList.remove("scrolled");}
+});
